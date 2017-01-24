f4fd7e2a1e6d04ed7c2b2b72288138846711ee39
Leonardo Gatica <lgatica@protonmail.com>
2017-01-24 00:52:42 -0300

    fix(python): Fix print

--- mongouri.py
+++ mongouri.py
@@ -8,11 +8,10 @@ uri = os.environ['MONGO_URI']
 data = pymongo.uri_parser.parse_uri(uri)
 
 if len(sys.argv) == 2:
-  print data['database']
+  print(data['database'])
 else:
   auth = '' if data['username'] is None else '-u %s -p %s' % (data['username'], data['password'])
   host = '-h %s:%s' % (data['nodelist'][0][0], data['nodelist'][0][1])
   dbname = '-d %s' % data['database'] if os.environ.get('MONGO_COMPLETE') is None else ''
   options = '%s %s %s' % (auth, host, dbname)
-
-  print options
+  print(options)
