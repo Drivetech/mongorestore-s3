0c80b2311496e26d51b7c2bf1d2942c13b7f0ac2
Leonardo Gatica <lgatica@protonmail.com>
2017-01-24 00:56:02 -0300

    feat(mongo): Add multiple inputs and extra options. Remove keep backups

--- restore.sh
+++ restore.sh
@@ -2,31 +2,30 @@
 
 OPTIONS=`python /usr/local/bin/mongouri`
 DB_NAME=`python /usr/local/bin/mongouri database`
+EXTRA_OPTIONS=${EXTRA:-}
 
-# Download backup
-aws s3 cp "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}" "/backup/${BACKUP_NAME}"
-# Decompress backup
-cd /backup/ && tar xzvf $BACKUP_NAME
+IFS=","
+for BACKUP_NAME in $BACKUP_NAMES
+do
+  # Download backup
+  aws s3 cp "s3://${S3_BUCKET}/${S3_PATH}/${BACKUP_NAME}" "/backup/${BACKUP_NAME}"
+  # Decompress backup
+  cd /backup/ && tar xzvf $BACKUP_NAME
 
-# Run backup
-if [ -n "${COLLECTIONS}" ]; then
-  echo $COLLECTIONS | sed -n 1'p' | tr ',' '\n' | while read collection; do
-    mongorestore ${OPTIONS} -c ${collection} "/backup/dump/${DB_NAME}/${collection}.bson"
-  done
-else
-  mongorestore ${OPTIONS} "/backup/dump/${DB_NAME}"
-fi
+  # Run backup
+  if [ -n "${COLLECTIONS}" ]; then
+    for COLLECTION in $COLLECTIONS
+    do
+      mongorestore ${OPTIONS} -c ${COLLECTION} ${EXTRA_OPTIONS} "/backup/dump/${DB_NAME}/${COLLECTION}.bson"
+    done
+  else
+    mongorestore ${OPTIONS} ${EXTRA_OPTIONS} "/backup/dump/${DB_NAME}"
+  fi
 
-# Delete temp files
-rm -rf /tmp/dump
+  # Delete temp files
+  rm -rf /tmp/dump
 
-# Delete backup files
-if [ -n "${MAX_BACKUPS}" ]; then
-  while [ $(ls /backup -w 1 | wc -l) -gt ${MAX_BACKUPS} ];
-  do
-    BACKUP_TO_BE_DELETED=$(ls /backup -w 1 | sort | head -n 1)
-    rm -rf /backup/${BACKUP_TO_BE_DELETED}
-  done
-else
+  # Delete backup files
   rm -rf /backup/*
-fi
+done
+
